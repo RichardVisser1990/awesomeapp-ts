adf29d62e099ac8b1404e6d43cbf623df5cb69d0
richard visser <richard.visser@kandoor.nl>
2022-07-04 16:43:03 +0200

    Some Pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+import Button from "./components/Button";
 
-function App() {
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hoi dit is de website van richard.
-        </a>
-      </header>
+      <main>
+        <Button />
+        <Title content="Pokemons" />
+        {all_pokemon.map((pokemon) => {
+          return (
+            <Pokemon
+              name={pokemon.name}
+              weight={pokemon.weight}
+              awesome={pokemon.awesome}
+              terrifying={pokemon.terrifying}
+              abilities={pokemon.abilities}
+            />
+          );
+        })}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Title.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {
+  content: string;
+};
+
+export default function Title({ content }: Props) {
+  return <h1>{content}</h1>;
+}
