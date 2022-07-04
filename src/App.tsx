564e5ee8f33bdc12105c8510f4688cd3fd5cb2ae
richard visser <richard.visser@kandoor.nl>
2022-07-04 14:16:10 +0200

    Some random changes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello world
+          Hoi dit is de website van richard.
         </a>
       </header>
     </div>
